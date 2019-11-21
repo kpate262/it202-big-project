248ba3755c0d5a2f6d9333767c8c7270a74b3967
Kisan Patel <kpate262@uic.edu>
2019-11-20 22:17:42 -0600

    Dynamically adds stock cards to home page

--- main.js
+++ main.js
@@ -21,7 +21,6 @@ var typeOfTimeSeries = { "everyFiveMin": "TIME_SERIES_INTRADAY", "daily":"TIME_S
                   https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=BA&apikey=demo
 */
 
-
 /*
   News courtsy of News Api
   link: https://newsapi.org/
@@ -39,3 +38,27 @@ var newsToDate = "&to="
                   https://newsapi.org/v2/everything?q=apple&from=2019-11-18&to=2019-11-18&sortBy=popularity&apiKey=4db8c37440f448848c0ee203d91c5c42
                   https://newsapi.org/v2/everything?q=bitcoin&apiKey=4db8c37440f448848c0ee203d91c5c42
 */
+
+var favoriteStocks = {"SBUX":["Starbucks Corporation"], "AAPL": ["Apple Inc."], "LYFT":["Lyft Inc."], "INTC":["Intel Corporation"],
+                      "TWTR":["Twitter Inc."], "GOOGL":["Alphabet Inc."], "AMZN":["Amazon.com Inc."]};
+
+
+$(document).ready(function(){
+  const keys = Object.keys(favoriteStocks);
+  for(const key of keys){
+    console.log(key);
+    $(".home").append('<div class="mdc-card demo-card demo-ui-control">' +
+                        '<div class="mdc-card__primary-action demo-card__primary-action favCard" tabindex="0">' +
+                          '<div class="demo-card__primary">'+
+                            '<h2 class="demo-card__title mdc-typography mdc-typography--headline6 symbol">'+ key +'</h2>' +
+                            '<h3 class="demo-card__subtitle mdc-typography mdc-typography--subtitle2 companyName">'+ favoriteStocks[key][0] +'</h3>' +
+                          '</div>' +
+                          '<span class="mdc-tab-indicator mdc-tab-indicator--active">' +
+                            '<span class="mdc-tab-indicator__content mdc-tab-indicator__content--underline"></span>' +
+                          '</span>' +
+                          '<span class="mdc-tab__ripple"></span>' +
+                        '</div>' +
+                      '</div>');
+
+  }
+});
